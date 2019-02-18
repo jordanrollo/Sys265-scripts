15f1d867ed20172cc8103920b9c6e94d0aabd20d
root <root@localhost.localdomain>
2019-02-18 11:49:57 -0500

    Organized Directories

---
+++ linux/centos7/secure-ssh.sh
@@ -0,0 +1,8 @@
+#SECURE-SSH.SH
+#AUTHOR JORDAN ROLLO
+#Creates a new ssh user called $1
+#Adds a public key to that users authorized keys file
+#Removes rooots ability to ssh in
+
+echo "testing 123"
+
