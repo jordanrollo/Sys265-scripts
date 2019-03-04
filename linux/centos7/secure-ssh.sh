4795c07e3c4fb89ee8a07c1aa9f9d5cf3da9d70c
root <root@linux2.jordan.local>
2019-03-04 10:58:18 -0500

    asdf

--- linux/centos7/secure-ssh.sh
+++
@@ -1,8 +0,0 @@
-#SECURE-SSH.SH
-#AUTHOR JORDAN ROLLO
-#Creates a new ssh user called $1
-#Adds a public key to that users authorized keys file
-#Removes rooots ability to ssh in
-
-echo "testing 123"
-
